bfbeffce3ff44c3b517ebd12225bd6c5faca1174
malong <malong@17xiaoshou.com>
2021-04-24 12:04:12 +0800

    初始化传入jaeger config option

--- jaegerc/jaegerc.go
+++ jaegerc/jaegerc.go
@@ -2,11 +2,10 @@ package jaegerc
 
 import (
 	"fmt"
-	"io"
-
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"io"
 )
 
 type JaegerType string
@@ -20,7 +19,7 @@ const (
 
 // NewJaegerTracer 初始化jaeger client
 // jaegerType 默认使用 const  param 1 全采样 0 不采样 0.5 50%采样
-func NewJaegerTracer(jaegerHostPort, appName string, jaegerType JaegerType, param float64) (opentracing.Tracer, io.Closer) {
+func NewJaegerTracer(jaegerHostPort, appName string, jaegerType JaegerType, param float64, logger jaeger.Logger) (opentracing.Tracer, io.Closer) {
 	cfg := &jaegercfg.Configuration{
 		Sampler: &jaegercfg.SamplerConfig{
 			Type:  string(jaegerType), //固定采样
@@ -33,9 +32,8 @@ func NewJaegerTracer(jaegerHostPort, appName string, jaegerType JaegerType, para
 
 		ServiceName: appName,
 	}
-
 	// tracer, closer, err := cfg.NewTracer(jaegercfg.Logger(jaeger.StdLogger))
-	tracer, closer, err := cfg.NewTracer(jaegercfg.Logger(jaeger.NullLogger))
+	tracer, closer, err := cfg.NewTracer(jaegercfg.Logger(logger))
 	if err != nil {
 		fmt.Printf("ERROR: cannot init Jaeger: %v\n", err)
 	}
